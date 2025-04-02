6a8ffdba541b606bfc5f22308d2eab7f93546227
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2025-04-02 23:24:48 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, GitHub Actions!");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
